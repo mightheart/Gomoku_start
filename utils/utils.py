518ac87415f14424ad816718942a15ed0af2285d
Infstellar <2644641825@qq.com>
2025-06-05 16:28:53 +0800

    添加计时器装饰器以监测函数执行时间，更新依赖项

--- utils/utils.py
+++ utils/utils.py
@@ -4,6 +4,8 @@
 import pygame
 import os
 from utils.constants import *
+import time
+from functools import wraps
 
 
 def set_working_directory():
@@ -48,3 +50,15 @@ def get_board_position_from_mouse(mouse_pos, board_x, board_y, board_size):
         if 0 <= row < board_size and 0 <= col < board_size:
             return row, col
     return None, None
+
+def timer(func):
+    """监测函数运行时间的装饰器"""
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        result = func(*args, **kwargs)
+        end_time = time.time()
+        execution_time = end_time - start_time
+        print(f"函数 {func.__name__} 执行时间: {execution_time:.4f} 秒")
+        return result
+    return wrapper

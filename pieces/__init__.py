0e4ae2234207feaef45a0607a11a560cbb79e774
{Elfin1988} <{18071340642@163.com}>
2025-06-07 15:00:23 +0800

    删掉了国际象棋的棋子定义和相关函数

--- pieces/__init__.py
+++ pieces/__init__.py
@@ -1,20 +1,13 @@
 """棋子模块
 
-定义国际象棋中的各种棋子类型
+定义棋子类型
 """
 
 from .chess_pieces import (
-    Piece, Pawn, King, Queen, Bishop, Knight, Rook,
-    PIECE_ORDER
+    Piece, Pawn
 )
 
 __all__ = [
     'Piece',       # 基类
-    'Pawn',        # 兵
-    'King',        # 王
-    'Queen',       # 后
-    'Bishop',      # 象
-    'Knight',      # 马
-    'Rook',        # 车
-    'PIECE_ORDER'  # 棋子顺序
+    'Pawn',        # 棋子初始类型
 ]

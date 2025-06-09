b2c59523a2f464469f542fc9df927621e0b7878b
ZHJ <202464820255@mail.scut.edu.cn>
2025-06-09 23:31:17 +0800

    把星空分出去了，加了个CSGO模式

--- 1.py
+++
@@ -1,9 +0,0 @@
-import pygame
-
-pygame.init()
-background_path = "e:/python/python/Gomoku_start-2/models/background1.jpg"
-try:
-    background_image = pygame.image.load(background_path)
-    print("图片加载成功")
-except Exception as e:
-    print(f"图片加载失败: {e}")
